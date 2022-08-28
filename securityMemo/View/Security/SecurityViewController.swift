41860d8d4cd640b5596ce6bdf35e936abf3e51b4
Haeseong <hesung0805@gmail.com>
2022-08-28 14:38:23 +0900

    1

---
+++ securityMemo/View/Security/SecurityViewController.swift
@@ -0,0 +1,62 @@
+//
+//  SecurityViewController.swift
+//  securityMemo
+//
+//  Created by haeseongJung on 2022/08/28.
+//
+
+import UIKit
+
+class SecurityViewController: UIViewController {
+    
+    var password: String = ""
+    
+    @IBOutlet weak var oneLabel: UILabel!
+    @IBOutlet weak var twoLabel: UILabel!
+    @IBOutlet weak var threeLabel: UILabel!
+    @IBOutlet weak var fourLabel: UILabel!
+    
+    @IBOutlet weak var numberPadSeven: UIButton!
+    @IBOutlet weak var numberPadEight: UIButton!
+    @IBOutlet weak var numberPadNine: UIButton!
+    @IBOutlet weak var numberPadFour: UIButton!
+    @IBOutlet weak var numberPadFive: UIButton!
+    @IBOutlet weak var numberPadSix: UIButton!
+    @IBOutlet weak var numberPadOne: UIButton!
+    @IBOutlet weak var numberPadTwo: UIButton!
+    @IBOutlet weak var numberPadThree: UIButton!
+    @IBOutlet weak var numberPadZero: UIButton!
+    
+    @IBOutlet weak var clearButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func numberPadButtonTapped(_ sender: UIButton) {
+        if sender.tag == 10 {
+            oneLabel.text = "-"
+            twoLabel.text = "-"
+            threeLabel.text = "-"
+            fourLabel.text = "-"
+            password = ""
+        } else {
+            if password.count == 0 {
+                oneLabel.text = "*"
+                password = "\(sender.tag)"
+            } else if password.count == 1 {
+                twoLabel.text = "*"
+                password = "\(password)\(sender.tag)"
+            } else if password.count == 2 {
+                threeLabel.text = "*"
+                password = "\(password)\(sender.tag)"
+            } else if password.count == 3 {
+                fourLabel.text = "*"
+                password = "\(password)\(sender.tag)"
+            }
+        }
+        print(password)
+    }
+    
+}
