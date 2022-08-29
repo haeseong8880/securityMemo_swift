27b413a6360bd5220041b310362b5404074aa439
Haeseong <hesung0805@gmail.com>
2022-08-29 19:49:58 +0900

    gitignore 추가

---
+++ securityMemo/View/Home/Cell/MemoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoTableViewCell.swift
+//  securityMemo
+//
+//  Created by haeseongJung on 2022/08/29.
+//
+
+import UIKit
+
+class MemoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- securityMemo/View/Home/HomeViewController.swift
+++ securityMemo/View/Home/HomeViewController.swift
@@ -6,11 +6,63 @@
 //
 
 import UIKit
+import FSCalendar
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var calendarView: FSCalendar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.calendarViewConfigure()
+        
+        calendarView.delegate = self
+        calendarView.dataSource = self
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+}
+
+
+//MARK: - Configure
+extension HomeViewController {
+    private func calendarViewConfigure() {
+        calendarView.backgroundColor = UIColor(named: "Ivory")
+        calendarView.appearance.headerTitleColor = UIColor(named: "Color3")
+        calendarView.appearance.weekdayTextColor = UIColor(named: "Color3")
+    }
+}
+
+//MARK: - TableviewDataSource
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MemoTableViewCell") as? MemoTableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+}
+
+//MARK: - TableViewDelegate
+extension HomeViewController: UITableViewDelegate {
+    
+}
+
+//MARK: - FSCalendarDelegate
+extension HomeViewController: FSCalendarDelegate {
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        print(dateFormatter.string(from: date) + " 선택됨")
+    }
+}
+//MARK: - FSCalendarDataSource
+extension HomeViewController: FSCalendarDataSource {
+    
 }
